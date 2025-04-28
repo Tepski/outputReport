153176a1fe20086074d60507eca0a3a3caedb979
Tepski <russeljoseph28@gmail.com>
2025-04-29 06:08:41 +0800

    Moved to PostgresDB

--- report/settings.py
+++ report/settings.py
@@ -68,23 +68,23 @@ WSGI_APPLICATION = 'report.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'output_report_db',
-#         'USER': 'tepskiii',
-#         'PASSWORD': 'cs6fo63jHhgh7eExU9rVEKIwbRcIEV8u',
-#         'HOST': 'dpg-d07j1rhr0fns738k5e3g-a',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'output_report_db',
+        'USER': 'tepskiii',
+        'PASSWORD': 'cs6fo63jHhgh7eExU9rVEKIwbRcIEV8u',
+        'HOST': 'dpg-d07j1rhr0fns738k5e3g-a',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
